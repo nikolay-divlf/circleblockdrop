7d002abed646cfc0fbebff1d7d6c3e618f87cc9b
nikolay-divlf <nikol73333333@yandex.ru>
2025-08-31 21:27:01 +0400

    Update

--- dev/plugin/circleblockdrop.js
+++ dev/plugin/circleblockdrop.js
@@ -1,9 +1,3 @@
-/**!
- * circleblockdrop v2.0 | divleaf.ru | https://github.com/nikolay-divlf/circleblockdrop
- * @author Goryachev Nikolay
- * @copyright NG 29-08-2025
- * @license MIT
- */
 (function ($) {
     var name_plugin = 'circleblockdrop';
     var base = {
